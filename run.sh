64b39ed60795e4dcda920adb39adca34bed19f78
alpkabac <oktayalpkabac@gmail.com>
2021-12-13 13:05:08 +0300

    learning git

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 cd ~/.local/lib/python3.8/site-packages/parlai/chat_service/services/websocket
-python3 run.py --config-path ../../tasks/chatbot/config2.yml --port 8081
-python3 api.py
-python3 telegram_bot.py
+python3 run.py --config-path ../../tasks/chatbot/config2.yml --port 8081 &
